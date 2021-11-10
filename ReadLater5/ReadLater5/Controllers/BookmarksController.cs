3b3ab8f5acaa034c9100e68d00ecef7637ad3972
Ffivebit <sandpar@live.com>
2021-11-10 09:39:38 +0100

    Add project files.

---
+++ ReadLater5/ReadLater5/Controllers/BookmarksController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Services;
+using Entity;
+
+namespace ReadLater5.Controllers
+{
+    public class BookmarksController : Controller
+    {
+        IBookmarkService _bookmarkService;
+        public BookmarksController(IBookmarkService bookmarkService)
+        {
+            _bookmarkService = bookmarkService;
+        }
+
+        // GET: Bookmarks
+        public IActionResult Index()
+        {
+            List<Bookmark> model = _bookmarkService.GetBookmarks();
+            return View(model);
+        }
+
+        // GET: Bookmarks/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+
+        }
+
+        // GET: Bookmarks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Bookmarks/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookmarkService.CreateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // POST: Bookmarks/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Bookmark bookmark)
+        {
+            if (ModelState.IsValid)
+            {
+                _bookmarkService.UpdateBookmark(bookmark);
+                return RedirectToAction("Index");
+            }
+            return View(bookmark);
+        }
+
+        // GET: Bookmarks/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status400BadRequest);
+            }
+            Bookmark bookmark = _bookmarkService.GetBookmark((int)id);
+            if (bookmark == null)
+            {
+                return new StatusCodeResult(Microsoft.AspNetCore.Http.StatusCodes.Status404NotFound);
+            }
+            return View(bookmark);
+        }
+
+        // POST: Bookmarks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Bookmark bookmark = _bookmarkService.GetBookmark(id);
+            _bookmarkService.DeleteBookmark(bookmark);
+            return RedirectToAction("Index");
+        }
+    }
+}
